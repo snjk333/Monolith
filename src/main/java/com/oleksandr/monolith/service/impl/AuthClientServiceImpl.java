949a6d6c5adef7d92aadd560e5d8fca28e9aa98a
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2025-09-05 15:49:46 +0200

    Add Service Annotation for DI

--- src/main/java/com/oleksandr/monolith/service/impl/AuthClientServiceImpl.java
+++ src/main/java/com/oleksandr/monolith/service/impl/AuthClientServiceImpl.java
@@ -1,6 +1,8 @@
 package com.oleksandr.monolith.service.impl;
 
 import com.oleksandr.monolith.service.interfaces.AuthClientService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthClientServiceImpl implements AuthClientService {
 }
