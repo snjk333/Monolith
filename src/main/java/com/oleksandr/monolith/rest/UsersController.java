5a83ea1fcbcdb457a9b01718448a273caed7f8f1
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2025-09-24 16:46:48 +0200

    start user controller idea

--- src/main/java/com/oleksandr/monolith/rest/UsersController.java
+++ src/main/java/com/oleksandr/monolith/rest/UsersController.java
@@ -1,11 +1,45 @@
 package com.oleksandr.monolith.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.oleksandr.monolith.Booking.DTO.BookingSummaryDTO;
+import com.oleksandr.monolith.User.DTO.UserDTO;
+import com.oleksandr.monolith.User.DTO.UserSummaryDTO;
+import com.oleksandr.monolith.User.DTO.UserUpdateRequestDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+
+    // GET /users/{id} → получить профиль пользователя (детально)
+    @GetMapping("/{id}")
+    public UserDTO getUserProfile(@PathVariable UUID id) {
+        // TODO: сервис вытаскивает юзера и маппит в UserDTO
+        return null;
+    }
+
+    // PATCH /users/{id} → обновить профиль
+    @PatchMapping("/{id}")
+    public UserSummaryDTO updateUserProfile(
+            @PathVariable UUID id,
+            @RequestBody UserUpdateRequestDTO request
+    ) {
+        // TODO: сервис апдейтит username/email и возвращает summary
+        return null;
+    }
+
+    // GET /users/{id}/bookings → список бронирований пользователя
+    @GetMapping("/{id}/bookings")
+    public List<BookingSummaryDTO> getUserBookings(@PathVariable UUID id) {
+        // TODO: сервис отдаёт список броней этого юзера
+        return null;
+    }
+
+
+
+
 }
 
 
